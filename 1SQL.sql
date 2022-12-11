1b6eef1b3e277767ec8a66b554d24082a24aa87d
şahin aydoğdu <117470052+sahin160@users.noreply.github.com>
2022-12-11 16:23:52 +0300

    OK

---
+++ 1SQL.sql
@@ -0,0 +1,5 @@
+select title,description from film
+select * from film where length>60 and length<75
+select * from film where rental_rate=0.99 and (replacement_cost =12.99 or replacement_cost=28.99)
+select last_name from customer where first_name='Mary'
+select * from film where length<50 and rental_rate not between 2.99 and 4.99
